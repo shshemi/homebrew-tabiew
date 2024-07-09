e88f9cdcf73af8f3e6ab4282f0e4b7293e76b9f3
Shayan Hashemi <shshemi@gmail.com>
2024-07-09 21:23:24 +0300

    v0.5.0

--- Formula/tabiew.rb
+++ Formula/tabiew.rb
@@ -1,10 +1,10 @@
 class Tabiew < Formula
     desc "Tabiew is a lightweight, terminal-based application to view and query delimiter separated value formatted documents, such as CSV and TSV files."
     homepage "https://github.com/shshemi/tabiew"
-    url "https://github.com/shshemi/tabiew/archive/refs/tags/v0.4.3.tar.gz"
-    version "0.4.3"
+    url "https://github.com/shshemi/tabiew/archive/refs/tags/v0.5.0.tar.gz"
+    version "0.5.0"
     license "MIT"
-    sha256 "a56b927ea4d9be3fa4732ad0c280b65c656e1445bd3d318c23a0f15aafa795a6"
+    sha256 "ecf97c79594bf633f575c2ee9a656063e474ddb221ba6704e57252d4ff2f046f"
   
     depends_on "rust" => :build
   
