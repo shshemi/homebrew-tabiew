4c9d50b4dde21eab6c3d21fa651786980f1b379c
Shayan Hashemi <shshemi@gmail.com>
2025-01-03 00:02:33 +0200

    v0.8.1

--- Formula/tabiew.rb
+++ Formula/tabiew.rb
@@ -1,10 +1,10 @@
 class Tabiew < Formula
     desc "Tabiew is a lightweight TUI application to view and query tabular data files, such as CSV, TSV, and parquet."
     homepage "https://github.com/shshemi/tabiew"
-    url "https://github.com/shshemi/tabiew/archive/refs/tags/v0.8.0.tar.gz"
-    version "0.8.0"
+    url "https://github.com/shshemi/tabiew/archive/refs/tags/v0.8.1.tar.gz"
+    version "0.8.1"
     license "MIT"
-    sha256 "d8f5a7ab8373d8cb1ca88a8d921f0ce0f44ff34bf5fdbf6afd170594ba28df9a"
+    sha256 "912c45cf26d83b96a0003c0134c2971c82cbdf2dda630fa8fa11e41c4ce28150"
   
     depends_on "rust" => :build
   
